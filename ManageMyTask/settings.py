3f827d81d87c29b224ba9e921eca80dee261ce24
Harshit <2000520310026@ietlucknow.ac.in>
2023-07-27 16:52:00 +0530

    Done vercel setting

--- ManageMyTask/settings.py
+++ ManageMyTask/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS =[
     "static",
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', static)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
